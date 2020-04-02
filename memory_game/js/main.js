19189b52e2c8fd0af6ba3c641337c398fd615b2b
Mona Parnian <monaparnian@Monas-Air.hub>
2020-04-02 11:51:13 +1100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up & Running!");
+let cardOne = "king";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "queen";
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);

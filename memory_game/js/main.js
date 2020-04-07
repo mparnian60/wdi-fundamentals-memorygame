658ec982e6af76b32490c6e46925c10ed8fc1a40
Mona Parnian <monaparnian@Monas-Air.hub>
2020-04-07 13:48:28 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-console.log("Up & Running!");
-let cardOne = "king";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "queen";
+let cards = ["queen", "queen", "king", "king"];
+let cardInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[1];
+cardInPlay.push(cardOne);
+cardInPlay.push(cardTwo);
 console.log("user flipped " + cardOne);
 console.log("user flipped " + cardTwo);
+if(cardInPlay.length === 2) {
+	if(cardInPlay[0] ===cardInPlay[1]) {
+		alert("you found a match!");
+	}
+	else{
+		alert("sorry, try again");
+	}
+}
+	

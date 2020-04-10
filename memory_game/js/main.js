891fe47847ef32766fb8babfa7dbdf34479b2295
Mona Parnian <monaparnian@Monas-Air.hub>
2020-04-10 17:48:03 +1000

    created game lodic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-let cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+}
+];
 let cardInPlay = [];
 
 function checkForMatch(){
@@ -9,8 +30,10 @@ function checkForMatch(){
 }
 };
 function flipCard(cardId){
-console.log("User flipped " + cards[cardId]),
-cardInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+cardInPlay.push(cards[cardId].rank);
 };
 
 flipCard(0);
